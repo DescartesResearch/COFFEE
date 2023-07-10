a3ab841eebeb34be47e0578d46a5507db4514816
martin.straesser <26547750+martinstraesser@users.noreply.github.com>
2023-07-10 15:31:51 +0200

    Bug fix

--- controller/src/main/java/tools/descartes/coffee/controller/monitoring/database/models/LoadDistribution.java
+++ controller/src/main/java/tools/descartes/coffee/controller/monitoring/database/models/LoadDistribution.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
+import java.util.List;
 
 @Entity
 public class LoadDistribution {
